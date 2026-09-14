58db4146e23b7599e15f82b02a56c1677b95cff1
Sahilarora83 <APNA_ASLI_EMAIL_YAHAN_DAALEIN>
2026-09-14 14:35:18 +0530

    chore: remove unrelated OS cleanup and autodesk scripts

--- RUN-FIX-AS-ADMIN.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-:: Double-click this file to run the cleanup as Administrator
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0fix-laptop-bloat.ps1\"'"
--- RUN-UNINSTALL-AUTODESK.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%~dp0uninstall-autodesk.ps1\"'"
